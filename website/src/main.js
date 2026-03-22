6937b096f011642da541483a4266e769b79e24d7
Jakub Pomykała <jakub.pomykala@gmail.com>
2026-03-22 16:06:43 +0100

    Update website

--- website/src/main.js
+++ website/src/main.js
@@ -2,5 +2,11 @@ import './style.css'
 import { renderNav } from './components/nav.js'
 import { renderFooter } from './components/footer.js'
 
+const DOWNLOAD_URL = 'https://github.com/jpomykala/NotchPrompter/releases/download/2.0.3/notch-prompter.app.zip'
+
 renderNav()
 renderFooter()
+
+document.querySelectorAll('[data-download-link]').forEach(el => {
+  el.href = DOWNLOAD_URL
+})
